8ecb271fadd7e2d87efebff4b4a99bd5090721b6
Vinaybhushana Reddy <vinay@caratred.com>
2020-11-01 12:00:25 +0530

    added users model and added admin role user with seed file

---
+++ models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  /* Name of the user */
+  fullName: {
+    type: String,
+    required: true
+  },
+  /* Email of the user */
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+  },
+  /* bcrypted password of the user */
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  /* role of the user */
+  role: {
+    type: String,
+    required: true
+  },
+  /* record created date */
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  /* record updated date */
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+    versionKey: false
+  });
+
+
+module.exports = mongoose.model('User', UserSchema);
--- seeds/users.js
+++ seeds/users.js
@@ -1,6 +1,6 @@
 module.exports = [{
     role: 'admin',
-    fullname: 'Vinay Reddy',
+    fullName: 'Vinay Reddy',
     email: 'cvbreddy.chedulla@gmail.com',
     hashedPassword: '$2b$10$ZXPQ7fLQfVjECRxxJ69EUOCp.VGKwjzAKSH.72hNbNrWX9hVJgAiC'
 }]
