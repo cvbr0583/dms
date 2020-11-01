a222c0613f7aff7cf000b50d8f9752cd4da70820
Vinaybhushana Reddy <vinay@caratred.com>
2020-11-01 11:54:29 +0530

    set up with seed data for admin user

---
+++ seed.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"undefined": "localhost/dms",
+	"dev": "localhost/DEV_DB_NAME",
+	"prod": "localhost/PROD_DB_NAME"
+}
---
+++ seeds/users.js
@@ -0,0 +1,6 @@
+module.exports = [{
+    role: 'admin',
+    fullname: 'Vinay Reddy',
+    email: 'cvbreddy.chedulla@gmail.com',
+    hashedPassword: '$2b$10$ZXPQ7fLQfVjECRxxJ69EUOCp.VGKwjzAKSH.72hNbNrWX9hVJgAiC'
+}]
